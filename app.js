9f3ad284eed421d21b32473d5428a551beb30cc0
Ananya <ananya.urlam@winwire.com>
2026-08-13 12:37:47 +0530

    index.html commited

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('ClickMe');
+    button.addEventListener("click",() => {
+        alert("Button clicked!");
+    });
+});
