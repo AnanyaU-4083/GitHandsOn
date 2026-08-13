64f827d8f18a1601a9b10131679726e54bc1e7bc
Ananya <ananya.urlam@winwire.com>
2026-08-13 14:37:37 +0530

    app.js commited

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('ClickMe');
     button.addEventListener("click",() => {
-        alert("Button clicked!");
+        alert("Button clicked!"); //this line will show an alert when the button is clicked 
     });
 });
